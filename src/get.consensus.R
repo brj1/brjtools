aeea0a9d64ccb00fe9e1d7be1cce4e67ecdd4274
brj1 <brj1@sfu.ca>
2018-05-29 15:12:11 -0700

    added get.concensus script

---
+++ src/get.consensus.R
@@ -0,0 +1,21 @@
+#!/usr/bin/Rscript
+
+get.mode <- function(x, dup=NA) {
+  tab <- table(x)
+  m <- max(tab)
+  if (!is.na(dup) & sum(tab == m) > 1)
+    dup
+  else
+    names(tab)[tab == m]
+}
+
+args <- commandArgs(trailingOnly = T)
+
+fasta.file <- args[1]
+fasta.conensus.file <- args[2]
+
+f <- read.fasta(fasta.file)
+df <- do.call(rbind, f)
+cons <- list(apply(df, 2, get.mode, '-'))
+
+write.fasta(cons, "CONCENSUS", fasta.concensus.file)
