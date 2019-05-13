fc606b51ab0ef7e5d35ba99ac24550af8d1f64db
brj1 <brj1@sfu.ca>
2019-05-13 14:06:18 -0700

    made hypermut default input/outut the same as LANL Hypermut 2.0; made hypermut a tad bit more user friendly;  added colour names to colourblind colour scheme '

--- cleaning/hypermut.R
+++ cleaning/hypermut.R
@@ -41,95 +41,166 @@ check.hyper <- function(ref, seq) {
 		pot.mut=length(pot.mut),
 		ctrl=ctrl,
 		pot.ctrl=length(pot.ctrl),
+		rate.ratio=(mut / length(pot.mut)) / (ctrl / length(pot.ctrl)),
 		p.value=fisher.test(m,alternative='greater')$p.value
+	)
+}
+
+try.open <- function(file.name, FOO, file.type, ...) {
+	tryCatch(
+		FOO(file.name, ...),
+		error=function() {
+			error(
+				sprintf(
+					"%s file '%s' does not exist or cannot be opened.",
+					file.type,
+					file.name
+				)
+			)
+		}
+	)
+}
+
+try.write <- function(x, file.name, FOO, file.type, ...) {
+	tryCatch(
+		FOO(x, file.name, ...),
+		error=function() {
+			error(
+				sprintf(
+					"Cannot create or write to %s file '%s'.",
+					file.type,
+					file.name
+				)
+			)
+		}
 	)
 }
 
 op <- OptionParser(
 	usage=
-"
+"\r            
 RHypermut: hypermutation detection.
 Detects APOBEC3-induced hypernutation in HIV sequences. Works the same as the LANL Hypermut progran (https://www.hiv.lanl.gov/content/sequence/HYPERMUT/hypermut.html).
 
-usage: %prog --fasta=FASTA [--cleanfasta=CLEANFASTA] [--hyperfasta=HYPERFASTA] [--log=LOG] [--makeconsensus] [--verbose]"
-)
-op <- add_option(
-	op,
-	"--fasta",
-	help="Input fasta file. If --makeconsensus is set the first sequence is used as a reference otherwise a concensus sequence is created to use as a reference.",
-	type='character'
+Usage: %prog [--cleanfasta=CLEANFASTA] [--hyperfasta=HYPERFASTA] [--log=LOG] [--ref=REF] [--threshold=THRESHOLD] [--makeconsensus] [--quiet] FASTA
+
+\tFASTA
+\t\tInput fasta file."
 )
 op <- add_option(
 	op,
 	"--cleanfasta",
-	help="Output fasta file with suspected hypermutated sequences removed.",
+	help="Output fasta file with suspected hypermutated sequences removed. Does not include the reference sequence.",
 	type='character',
 	default=NA
 )
 op <- add_option(
 	op,
 	"--hyperfasta",
-	help="Output fasta file containing the suspected hypermutated sequences. (Optional).",
+	help="Output fasta file containing the suspected hypermutated sequences.",
 	type='character',
 	default=NA
 )
 op <- add_option(
 	op,
 	"--log",
-	help="CSV file that contains the number of hypermutations, potential hypermutations, control mutations, potential control mutations and p-value for each sequence. (Optional).",
+	help="CSV file that contains the number of hypermutations, potential hypermutations, control mutations, potential control mutations and p-value for each sequence.",
 	type='character',
 	default=NA
 )
 op <- add_option(
 	op,
+	"--ref",
+	help="Either taxon name of the reference sequence in FASTA or a fasta file containing a single reference sequence. If --makeconcensus is specified this is not used. If not specified then the first sequence of FASTA is used as the reference. Ignored if --makeconsensus is set.",
+	type='character',
+	default='1'
+)
+op <- add_option(
+	op,
+	"--threshold",
+	help="The p-value threshold to assign sequences as hypermutated. Default: 0.05.",
+	type='numeric',
+	default='0.05'
+)
+op <- add_option(
+	op,
 	"--makeconsensus",
-	help="Set to use make a consensus sequence to use as a reference. (Optional).",
+	help="Set to make a consensus sequence to use as a reference.",
 	type='logical',
 	action='store_true',
 	default=F
 )
 op <- add_option(
 	op,
-	"--verbose",
-	help="Set to print the log file to screen. (Optional).",
+	"--quiet",
+	help="Set to suppress output.",
 	type='logical',
 	action='store_true',
 	default=F
 )
-args <- parse_args(op)
+all.args <- parse_args(op, positional_arguments=1)
+args <- all.args$options
 
-fasta.file <- args$fasta
+fasta.file <- all.args$args
 clean.fasta.file <- args$cleanfasta
 hyper.fasta.file <- args$hyperfasta
 log.file <- args$log
+ref.name <- args$ref
+threshold <- args$threshold
 make.consensus <- args$makeconsensus
-verbose <- args$verbose
+quiet <- args$quiet
 
-f <- lapply(read.fasta(fasta.file), tolower)
+f <- lapply(try.open(fasta.file, read.fasta, 'input fasta'), tolower)
 
 if (make.consensus) {
 	ref <- consensus(do.call(rbind, f))
 } else {
-	ref <- f[[1]]
-	f <- f[-1]
+	suppressWarnings(ref.num <- as.integer(ref.name))
+	if (!is.na(ref.num) && ref.num < names(f) && ref.num > 0) {
+		names(f)[ref.num] <- ref.name
+	}
+	
+	if (ref.name %in% names(f)) {
+		ref <- f[[ref.name]]
+		f <- f[names(f) != ref.name]
+	} else {
+		ref <- tolower(
+			try.open(ref.name, read.fasta, 'reference fasta')[[1]]
+		)
+	}
+}
+
+if (!all(sapply(f, length) == length(ref))) {
+	error("Sequence lengths differ. Make sure that the input fasta file is aligned and that the reference sequence is the same length.")
 }
 
 data <- do.call(rbind, lapply(f, check.hyper, ref=ref))
-
 p.value <- data$p.value
 
-if (verbose)
+if (!quiet)
 	print(data)
 
 if (!is.na(log.file))
-	write.csv(data, log.file)
+	try.write(data, log.file, write.csv, 'log')
 
 if (!is.na(clean.fasta.file)) {
-	f.clean <- f[p.value >= 0.05] 
-	write.fasta(f.clean, names(f.clean), clean.fasta.file)
+	f.clean <- f[p.value >= threshold] 
+	try.write(
+		f.clean,
+		names=names(f.clean),
+		clean.fasta.file,
+		write.fasta,
+		'clean output fasta'
+	)
 }
 
 if (!is.na(hyper.fasta.file)) {
-	f.hyper <- f[p.value < 0.05]
-	write.fasta(f.hyper, names(f.hyper), hyper.fasta.file)
+	f.hyper <- f[p.value < threshold]
+	try.write(
+		f.hyper,
+		names=names(f.hyper),
+		hyper.fasta.file,
+		write.fasta,
+		'hypermutated output fasta'
+	)
 }
--- plotting/colour_blind.R
+++ plotting/colour_blind.R
@@ -1,1 +1,11 @@
-colourblind <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
+colour.blind <- c(
+	"#000000", # black
+	"#E69F00", # orange
+	"#56B4E9", # blue
+	"#009E73", # green
+	"#CC79A7", # pink
+	"#F0E442", # yellow
+	"#0072B2", # dark blue
+	"#D55E00", # dark orange
+	"#999999" # grey (not in original palette)
+)
